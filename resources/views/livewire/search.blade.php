65e6ef380138983fbd76057b2d83d20093e43e60
Amrit Sinha <purpleplots@gmail.com>
2022-08-08 08:44:48 +0530

    Minor bug fixes - error response

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -19,21 +19,21 @@
             </button>
           <span class="inline animate-ping" wire:loading.delay.longer>⭕</span>
         </form>
+        
         @if (session()->has('message'))
-          <div class="alert alert-success">
+          <div class="alert alert-error">
             {{ session('message') }}
             </div>
         @endif
   </div>
-  
   </div>
     </div>
     <div class="flex justify-center md:block mt-8 md:mt-0 md:flex-end">
       <div class="relative">
         <img src='dist/assets/Highlight1.svg' alt="" class="absolute -top-16 -left-10" />
       </div>
-      <img class="animate-pulse justify-self-center @if($preview) hidden @endif" src='dist/assets/search-animation.gif' alt="Macbook" />
-      @if(isset($preview['url']))
+      <img class="animate-pulse justify-self-center @if(isset($data['url']) || isset($data['detail'])) hidden @endif" src='dist/assets/search-animation.gif' alt="Macbook" />
+      @if(isset($data['url']) && !isset($data['detail']))
       <div id="card" class="overflow-hidden shadow-lg rounded-lg h-90 w-60 md:w-80 cursor-pointer m-auto">
         <span href="{{$preview['url']}}" class="w-full block h-full">
           <img alt="opps! No cover image to show 😢" src="{{ $preview['image']['url']}}" class="max-h-40 w-full object-cover"/>
@@ -87,6 +87,35 @@
           </div>
         </span>
       </div>
+      @endif
+      @if(isset($data['detail']))
+      
+<div class="shadow-lg rounded-2xl p-4 bg-white dark:bg-gray-800 w-64 m-auto">
+    <div class="w-full h-full text-center">
+        <div class="flex h-full flex-col justify-between">
+<svg class="h-12 w-1/4 my-4 m-auto text-green-500" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 460.775 460.775" style="enable-background:new 0 0 460.775 460.775;" xml:space="preserve">
+<path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
+	c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
+	c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
+	c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
+	l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
+	c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
+</svg>
+            <span class="text-gray-800 dark:text-white font-bold">
+              Link Cannot Be Previewed!
+            </span>
+            <p class="text-gray-600 dark:text-gray-100 text-md py-2 px-6 text-justify">
+              {!! Str::words($data['detail'], 20, ' ...') !!}
+            </p>
+            <div class="flex items-center justify-between gap-4 w-full mt-8">
+                <button wire:click="resetValues" type="button" class="py-2 px-4  bg-black focus:ring-black-500 focus:ring-offset-indigo-200 text-secondary w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                    Close
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
       @endif
       <div id="howitworks" class="relative">
         <img src='dist/assets/Highlight2.svg' alt="" class="absolute -bottom-10 -right-0" />
